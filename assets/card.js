f50275647f5ba46a1f50d7c38a1cae82b3f1c9b6
supernova9sky <antoni.jasiukiewicz@gmail.com>
2025-04-17 17:19:56 +0200

    a

--- assets/card.js
+++ assets/card.js
@@ -1,43 +1,36 @@
-
-var confirmElement = document.querySelector(".confirm");
-
 function closePage(){
   clearClassList();
 }
 
 function openPage(page){
   clearClassList();
-  var classList = confirmElement.classList;
+  let classList = document.querySelector(".confirm").classList;
   classList.add("page_open");
   classList.add("page_" + page + "_open");
 }
 
 function clearClassList(){
-  var classList = confirmElement.classList;
-  classList.remove("page_open");
-  classList.remove("page_1_open");
-  classList.remove("page_2_open");
-  classList.remove("page_3_open");
+  let classList2 = document.querySelector(".confirm").classList;
+  classList2.remove("page_open");
+  classList2.remove("page_1_open");
+  classList2.remove("page_2_open");
+  classList2.remove("page_3_open");
 }
 
-var time = document.getElementById("time");
-var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+let time = document.getElementById("time");
+let options = { year: 'numeric', month: 'numeric', day: 'numeric' };
 
 if (localStorage.getItem("update") == null){
   localStorage.setItem("update", "16.04.2025")
 }
 
-var date = new Date();
+let date = new Date();
 
-var updateText = document.querySelector(".bottom_update_value");
-updateText.innerHTML = localStorage.getItem("update");
+let updateText = document.querySelector(".bottom_update_value");
+updateText.innerHTML = date.toLocaleDateString("pl-PL", options);
 
-var update = document.querySelector(".update");
+let update = document.querySelector(".update");
 update.addEventListener('click', () => {
-  var newDate = date.toLocaleDateString("pl-PL", options);
-  localStorage.setItem("update", newDate);
-  updateText.innerHTML = newDate;
-
   scroll(0, 0)
 });
 
@@ -54,7 +47,7 @@ function setClock(){
     })
 }
 
-var unfold = document.querySelector(".info_holder");
+let unfold = document.querySelector(".info_holder");
 unfold.addEventListener('click', () => {
 
   if (unfold.classList.contains("unfolded")){
@@ -65,29 +58,32 @@ unfold.addEventListener('click', () => {
 
 })
 
-var data = {}
+let data = {}
 
-var params = new URLSearchParams(window.location.search);
-for (var key of params.keys()){
-  data[key] = params.get(key);
+let parameter_object = new URLSearchParams(window.location.search);
+for (let key of parameter_object.keys()){
+  data[key] = parameter_object.get(key);
 }
 
 document.querySelector(".id_own_image").style.backgroundImage = `url(${data['image']})`;
 
-var birthday = data['birthday'];
-var birthdaySplit = birthday.split(".");
-var day = parseInt(birthdaySplit[0]);
-var month = parseInt(birthdaySplit[1]);
-var year = parseInt(birthdaySplit[2]);
+let birthday = data['birthday'];
+let birthdaySplit = birthday.split(".");
+let day = parseInt(birthdaySplit[0]);
+let month = parseInt(birthdaySplit[1]);
+let year = parseInt(birthdaySplit[2]);
 
-var birthdayDate = new Date();
+let birthdayDate = new Date();
 birthdayDate.setDate(day)
 birthdayDate.setMonth(month-1)
 birthdayDate.setFullYear(year)
 
 birthday = birthdayDate.toLocaleDateString("pl-PL", options);
+if (day < 9) {
+  birthday = "0" + birthday
+}
 
-var sex = data['sex'];
+let sex = data['sex'];
 
 if (sex === "m"){
   sex = "Mężczyzna"
@@ -106,13 +102,24 @@ setData("mothersFamilyName", data['mothersFamilyName']);
 setData("birthPlace", data['birthPlace']);
 setData("countryOfBirth", data['countryOfBirth']);
 setData("adress", "ul. " + data['adress1'] + "<br>" + data['adress2'] + " " + data['city']);
+setData("serial_number", "ZZC " + data["serial_number"]);
+setData("issuing_authority", data["issuing_authority"].toUpperCase());
+setData("expiry_date", data["expiry_date"]);
+
+setData("serial_number2", "ZZC " + data["serial_number"]);
+setData("issuing_authority2", data["issuing_authority"].toUpperCase());
+setData("expiry_date2", data["expiry_date"]);
+
+setData("issue_date", data["issue_date"]);
+setData("father_name", data["father_name"]);
+setData("mother_name", data["mother_name"]);
 
 if (localStorage.getItem("homeDate") == null){
-  var homeDay = getRandom(1, 25);
-  var homeMonth = getRandom(0, 12);
-  var homeYear = getRandom(2012, 2019);
+  let homeDay = getRandom(1, 25);
+  let homeMonth = getRandom(0, 12);
+  let homeYear = getRandom(2012, 2019);
 
-  var homeDate = new Date();
+  let homeDate = new Date();
   homeDate.setDate(homeDay);
   homeDate.setMonth(homeMonth);
   homeDate.setFullYear(homeYear)
@@ -126,7 +133,7 @@ if (parseInt(year) >= 2000){
   month = 20 + month;
 }
 
-var later;
+let later;
 
 if (sex.toLowerCase() === "mężczyzna"){
   later = "0295"
@@ -142,8 +149,8 @@ if (month < 10){
   month = "0" + month
 }
 
-var pesel = year.toString().substring(2) + month + day + later + "7";
-setData("pesel", "07210379137")
+let pesel = year.toString().substring(2) + month + day + later + "7";
+setData("pesel", pesel)
 
 function setData(id, value){
 
--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,10 @@
 
-var selector = document.querySelector(".selector_box");
+let selector = document.querySelector(".selector_box");
+
 selector.addEventListener('click', () => {
-    if (selector.classList.contains("selector_open")){
+    if (selector.classList.contains("selector_open")) {
         selector.classList.remove("selector_open")
-    }else{
+    } else {
         selector.classList.add("selector_open")
     }
 })
@@ -14,7 +15,7 @@ document.querySelectorAll(".date_input").forEach((element) => {
     })
 })
 
-var sex = "m"
+let sex = "m"
 
 document.querySelectorAll(".selector_option").forEach((option) => {
     option.addEventListener('click', () => {
@@ -23,15 +24,15 @@ document.querySelectorAll(".selector_option").forEach((option) => {
     })
 })
 
-var upload = document.querySelector(".upload");
+let upload = document.querySelector(".upload");
 
-var imageInput = document.createElement("input");
+let imageInput = document.createElement("input");
 imageInput.type = "file";
 imageInput.accept = ".jpeg,.png,.gif";
 
 document.querySelectorAll(".input_holder").forEach((element) => {
 
-    var input = element.querySelector(".input");
+    let input = element.querySelector(".input");
     input.addEventListener('click', () => {
         element.classList.remove("error_shown");
     })
@@ -50,105 +51,146 @@ imageInput.addEventListener('change', (event) => {
 
     upload.removeAttribute("selected")
 
-    var file = imageInput.files[0];
-    var data = new FormData();
+    let file = imageInput.files[0];
+    let data = new FormData();
     data.append("image", file);
 
-    fetch('	https://api.imgur.com/3/image' ,{
+    fetch('	https://api.imgur.com/3/image', {
         method: 'POST',
         headers: {
             'Authorization': 'Client-ID 546c25a59c58ad7'
         },
         body: data
     })
-    .then(result => result.json())
-    .then(response => {
-        
-        var url = response.data.link;
-        upload.classList.remove("error_shown")
-        upload.setAttribute("selected", url);
-        upload.classList.add("upload_loaded");
-        upload.classList.remove("upload_loading");
-        upload.querySelector(".upload_uploaded").src = url;
+        .then(result => result.json())
+        .then(response => {
 
-    })
+            let url = response.data.link;
+            upload.classList.remove("error_shown")
+            upload.setAttribute("selected", url);
+            upload.classList.add("upload_loaded");
+            upload.classList.remove("upload_loading");
+            upload.querySelector(".upload_uploaded").src = url;
+
+        })
 
 })
 
+function check_date(date_arr) {
+    let split_arr = date_arr.split(".");
+    
+    if (split_arr[0] > 31 || split_arr[0] < 1) {
+        return false;
+    }
+    
+    if (split_arr[1] > 12 || split_arr[1] < 1) {
+        return false;
+    }
+
+    if (split_arr[2] < 1000 || split_arr[2] > 9999) {
+        return false;
+    }
+
+    return true;
+}
+
 document.querySelector(".go").addEventListener('click', () => {
 
-    var empty = [];
+    let empty = [];
 
-    var params = new URLSearchParams();
+    let params = new URLSearchParams();
 
     params.set("sex", sex)
-    if (!upload.hasAttribute("selected")){
+    if (!upload.hasAttribute("selected")) {
         empty.push(upload);
         upload.classList.add("error_shown")
-    }else{
+    } else {
         params.set("image", upload.getAttribute("selected"))
     }
 
-    var birthday = "";
-    var dateEmpty = false;
+    let birthday = [];
     document.querySelectorAll(".date_input").forEach((element) => {
-        birthday = birthday + "." + element.value
-        if (isEmpty(element.value)){
-            dateEmpty = true;
-        }
+        birthday.push(element.value.toString());
     })
+    birthday = birthday.join(".");
 
-    birthday = birthday.substring(1);
+    let issue_date = [];
+    document.querySelectorAll(".issue_date_input").forEach((element) => {
+        issue_date.push(element.value.toString());
+    })
+    issue_date = issue_date.join(".");
 
-    if (dateEmpty){
-        var dateElement = document.querySelector(".date");
+    let expiry_date = [];
+    document.querySelectorAll(".expiry_date_input").forEach((element) => {
+        expiry_date.push(element.value.toString());
+    })
+    expiry_date = expiry_date.join(".");
+
+    if (birthday.length == 0 || check_date(birthday) == false) {
+        let dateElement = document.querySelector(".date");
         dateElement.classList.add("error_shown");
         empty.push(dateElement);
-    }else{
+    } else {
         params.set("birthday", birthday)
     }
 
+    if (issue_date.length == 0 || check_date(issue_date) == false) {
+        let dateElement = document.querySelector(".date2");
+        dateElement.classList.add("error_shown");
+        empty.push(dateElement);
+    } else {
+        params.set("issue_date", birthday)
+    }
+
+    if (expiry_date.length == 0 || check_date(expiry_date) == false) {
+        let dateElement = document.querySelector(".date3");
+        dateElement.classList.add("error_shown");
+        empty.push(dateElement);
+    } else {
+        params.set("expiry_date", birthday)
+    }
+    
+
     document.querySelectorAll(".input_holder").forEach((element) => {
 
-        var input = element.querySelector(".input");
+        let input = element.querySelector(".input");
 
-        if (isEmpty(input.value)){
+        if (isEmpty(input.value)) {
             empty.push(element);
             element.classList.add("error_shown");
-        }else{
+        } else {
             params.set(input.id, input.value)
         }
 
     })
 
-    if (empty.length != 0){
+    if (empty.length != 0) {
         empty[0].scrollIntoView();
-    }else{
-
+    } else {
         forwardToId(params);
     }
 
 });
 
-function isEmpty(value){
+function isEmpty(value) {
 
     let pattern = /^\s*$/
     return pattern.test(value);
 
 }
 
-function forwardToId(params){
+function forwardToId(params) {
 
     location.href = "/id?" + params
 
 }
 
-var guide = document.querySelector(".guide_holder");
+let guide = document.querySelector(".guide_holder");
 guide.addEventListener('click', () => {
 
-    if (guide.classList.contains("unfolded")){
+    if (guide.classList.contains("unfolded")) {
         guide.classList.remove("unfolded");
-    }else{
+    } else {
         guide.classList.add("unfolded");
     }
 
