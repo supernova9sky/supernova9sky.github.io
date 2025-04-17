9dd7f7ac5e64a38a778539a3b5d84488198627d0
supernova9sky <antoni.jasiukiewicz@gmail.com>
2025-04-17 14:59:24 +0200

    a

--- assets/id.js
+++ assets/id.js
@@ -1,73 +1,67 @@
-
-var params = new URLSearchParams(window.location.search);
-
-document.querySelector(".login").addEventListener('click', () => {
-    toHome();
-});
-
-var welcome = "Dzień dobry!";
-
-var date = new Date();
-if (date.getHours() >= 18){
-    welcome = "Dobry wieczór!"
-}
-document.querySelector(".welcome").innerHTML = welcome;
-
-function toHome(){
-    location.href = '/home?' + params;
-}
-
-var input = document.querySelector(".password_input");
-input.addEventListener("keypress", (event) => {
-    if (event.key === 'Enter') {
-        document.activeElement.blur();
-    }
-})
-
-var dot = "•";
-var original = "";
-var eye = document.querySelector(".eye");
-
-input.addEventListener("input", () => {
-    var value = input.value.toString();
-    var char = value.substring(value.length - 1);
-    if (value.length < original.length){
-        original = original.substring(0, original.length - 1);
-    }else{
-        original = original + char;
-    }
-
-    if (!eye.classList.contains("eye_close")){
-        var dots = "";
-        for (var i = 0; i < value.length - 1; i++){
-            dots = dots + dot
-        }
-        input.value = dots + char;
-        delay(3000).then(() => {
-            value = input.value;
-            if (value.length != 0){
-                input.value = value.substring(0, value.length - 1) + dot
-            }
-        });
-        console.log(original)
-    }
-})
-
-function delay(time, length) {
+function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
+function create_welcome_text(date) {
+    return (date.getHours() >= 18) ? "Dobry wieczór!" : "Dzień dobry!";
+}
+
+function toHome() {
+    location.href = '/home?' + new URLSearchParams(window.location.search);
+}
+
+function log_in(password) {
+    toHome();
+}
+
+
+document.querySelector(".welcome").innerHTML = create_welcome_text(new Date());
+
+let password_element = document.querySelector(".password_input");
+let dot = "•";
+let uncensored_password = "";
+let eye = document.querySelector(".eye");
+
+password_element.addEventListener("keydown", (event) => {
+    if (event.key === 'Enter') {
+        document.activeElement.blur();
+        log_in();
+    }
+})
+
+password_element.addEventListener("input", () => {
+    let password = password_element.value.toString();
+    let password_lenght = password.length;
+    let last_char_typed = password.at(-1);
+
+    uncensored_password += last_char_typed;
+
+    if (!eye.classList.contains("eye_close")) {
+        let dots = dot.repeat(password_lenght - 1);
+        password_element.value = dots + last_char_typed;
+        delay(3000).then(() => {
+            password_element.value = dot.repeat(password_element.value.toString().length);
+        })
+        //console.log(uncensored_password);
+    }
+})
+
+document.querySelector(".login").addEventListener('click', () => {
+    // verify login info (discord + custom set password by the customer)
+    toHome();
+});
+
 eye.addEventListener('click', () => {
     var classlist = eye.classList;
-    if (classlist.contains("eye_close")){
+    if (classlist.contains("eye_close")) {
         classlist.remove("eye_close");
         var dots = "";
-        for (var i = 0; i < input.value.length - 1; i++){
+        for (var i = 0; i < password_element.value.length - 1; i++) {
             dots = dots + dot
         }
-        input.value = dots;
-    }else{
+        password_element.value = dots;
+    } else {
         classlist.add("eye_close");
-        input.value = original;
+        password_element.value = uncensored_password;
     }
 })
