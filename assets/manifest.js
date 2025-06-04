c744a2cc76d82220ae73853f748997e01a297711
supernova9sky <antoni.jasiukiewicz@gmail.com>
2025-06-04 16:05:37 +0200

    abc

--- assets/manifest.js
+++ assets/manifest.js
@@ -10,3 +10,6 @@ let manifestElem = document.createElement('link');
 manifestElem.setAttribute('rel', 'manifest');
 manifestElem.setAttribute('href', 'data:application/manifest+json;base64,' + btoa(JSON.stringify(webManifest)));
 document.head.prepend(manifestElem);
+
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+document.documentElement.style.setProperty('--safari-back-rotation', isSafari ? '180deg' : '0deg');
